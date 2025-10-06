5979205d0db8319c9c206b3346b1d10378234ff3
Juanjosfc04 <juan.ramirez.rivera.al@iespoligonosur.org>
2025-10-06 17:55:43 +0000

    ej06

---
+++ Ej06.java
@@ -0,0 +1,12 @@
+public class Ej06{
+    public static void main (String[] args){
+        String nombreUsuario = "Juanjo";
+        int edadUsuario = 25;
+        String ciudadUsuario = "sevilla";
+        
+        System.out.println(nombreUsuario); 
+        System.out.println(edadUsuario);  
+        System.out.println(ciudadUsuario); 
+    }
+        
+}
