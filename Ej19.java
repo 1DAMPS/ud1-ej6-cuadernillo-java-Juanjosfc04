6072f56c7dd0748227c83c557a7916d4d4e35fb0
Juanjosfc04 <juan.ramirez.rivera.al@iespoligonosur.org>
2025-10-07 17:16:45 +0000

    Ej19.java

---
+++ Ej19.java
@@ -0,0 +1,12 @@
+public class Ej19 {
+    public static void main(String[] args) {
+        // Le damos un valor de 5 al radio del círculo
+        int radio = 5;
+
+        //  Calculamos el área usando la fórmula de siempre: pi * radio * radio
+        double area = 3.1416 * radio * radio;
+
+        //  Mostramos el resultado en la pantalla
+        System.out.println(area);
+    }
+}
