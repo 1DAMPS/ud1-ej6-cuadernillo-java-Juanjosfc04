282776dca032851f451075db8fd1d716842cc9bd
Juanjosfc04 <juan.ramirez.rivera.al@iespoligonosur.org>
2025-10-07 17:44:15 +0000

    Ej20

---
+++ Ej20.java
@@ -0,0 +1,11 @@
+/**
+ * @author jaunjo
+ * @version 1
+ */
+public class Ej20 {
+    public static void main(String[] args) {
+            int numero = 10;
+             int resultado = numero * 2;
+        System.out.println("El doble de " + numero + " es " + resultado);
+    }
+}
