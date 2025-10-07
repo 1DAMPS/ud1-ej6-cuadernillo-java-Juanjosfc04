0ac11d5852300d57ed78973284d2dbbca9924883
Juanjosfc04 <juan.ramirez.rivera.al@iespoligonosur.org>
2025-10-07 17:13:07 +0000

    Ej18.java

---
+++ Ej18.java
@@ -0,0 +1,17 @@
+// juanjo
+//  7-9-25
+// hacer un claculo facil
+public class Ej18 {
+    public static void main(String[] args) {
+            
+            //declaramos un muero 
+            
+            int numero = 10;
+
+            // vamos hacer qeu el nuermero multiplique por 2
+
+
+             int resultado = numero * 2;
+        System.out.println("El doble de " + numero + " es " + resultado);
+    }
+}
