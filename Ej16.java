cafd157402a656a2aaa852564404d06d9e22e3fc
Juanjosfc04 <juan.ramirez.rivera.al@iespoligonosur.org>
2025-10-07 17:03:53 +0000

    Ej16.java

---
+++ Ej16.java
@@ -0,0 +1,8 @@
+public class Ej16{
+public static void main (String [] args){
+    short valor = 30000;
+    short resultado = (short) ( valor * 10 );
+     
+     System.out.println ("Resutado:" + resultado);
+}
+}
